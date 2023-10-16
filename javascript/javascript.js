4828c72a47181f7177bda966576e85ef670774b4
teoma145 <matteodark145@yahoo.it>
2023-10-16 17:26:57 +0200

    fix-if

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,10 +3,12 @@ let counter=0;
 let oggettoutente = '';
     
 
-while(oggettoutente !=='stop' && counter < 20){
-  oggettoutente= prompt('ti serve altro per la spesa?');
-  listaspesa.push(oggettoutente);
-  counter++;
-}
+while (oggettoutente !== 'stop' && counter < 20) {
+  oggettoutente = prompt('Ti serve altro per la spesa?');
 
+  if (oggettoutente !== 'stop') {
+    listaspesa.push(oggettoutente);
+    counter++;
+}
+}
 console.log(listaspesa)
